224832c56904bc08885cd7068f961a6e69a0eb43
ryanmueller19 <59619801+ryanmueller19@users.noreply.github.com>
2020-09-11 13:10:14 -0400

    Activity 2

--- Magpie/src/App.java
+++ Magpie/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(17/ 5 % 3 + 17 * 5 % 3);
     }
 }
